5e2f1b6a3913d4f53509dfa6b02c6f128968d599
Maxime FABRE <mafabre@e2r3p3.42.fr>
2017-04-27 16:35:47 +0200

    fin initialisation game

--- srcs/init_arena.c
+++ srcs/init_arena.c
@@ -6,27 +6,35 @@
 /*   By: mafabre <mafabre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/26 20:49:40 by mafabre           #+#    #+#             */
-/*   Updated: 2017/04/26 23:28:37 by mafabre          ###   ########.fr       */
+/*   Updated: 2017/04/27 16:02:08 by mafabre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 #include "op.h"
 
-int		check_start(t_params *param)
+void	get_n(t_params *param)
 {
-	int i;
-
-	i = 0;
-	if (param->np == 1)
-		i = 0;
-	else if (param->np == 2)
-		i = MEM_SIZE * (param->n - 1) / 2;
-	else if (param->np == 3)
-		i = MEM_SIZE * (param->n - 1) / 3;
-	else if (param->np == 4)
-		i = MEM_SIZE * (param->n - 1) / 4;
-	return (i);
+	if (param->p1 != 1)
+	{
+		param->n = 1;
+		param->p1 = 1;
+	}
+	else if (param->p2 != 1)
+	{
+		param->n = 2;
+		param->p2 = 1;
+	}
+	else if (param->p3 != 1)
+	{
+		param->n = 3;
+		param->p3 = 1;
+	}
+	else if (param->p4 != 1)
+	{
+		param->n = 4;
+		param->p4 = 1;
+	}
 }
 
 void	send_in_arena(t_convert *tab, t_params *param, unsigned char *arena)
@@ -34,7 +42,10 @@ void	send_in_arena(t_convert *tab, t_params *param, unsigned char *arena)
 	int	i;
 	int	j;
 
-	i = check_start(param);
+	if (param->n == 0)
+		get_n(param);
+	printf("DDDDDDD %d\n", param->n);
+	i = MEM_SIZE * (param->n - 1) / param->np;
 	j = COMMENT_LENGTH/4 + PROG_NAME_LENGTH/4 + 4;
 	while (i < MEM_SIZE && j < COMMENT_LENGTH/4 + PROG_NAME_LENGTH/4 + CHAMP_MAX_SIZE/4 + 4)
 	{
