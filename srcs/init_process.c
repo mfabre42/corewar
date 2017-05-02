7cb9e6b68ee110658b5408a051b18c86839dda70
Alexis LEVEQUE <aleveque@e2r5p5.42.fr>
2017-05-02 17:31:14 +0200

    correction player nbr dans le registre

--- srcs/init_process.c
+++ srcs/init_process.c
@@ -6,7 +6,7 @@
 /*   By: aleveque <aleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/02 15:22:14 by aleveque          #+#    #+#             */
-/*   Updated: 2017/05/02 16:09:55 by aleveque         ###   ########.fr       */
+/*   Updated: 2017/05/02 17:30:50 by aleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ t_player	init_process(t_map *map, int player, int start)
 printf("player : %d\n", player);
 	tmp = map->player[player];
 	proc = ft_lstnew(start);
-	proc->reg[0] = (unsigned int)player;
+	proc->reg[0] = (unsigned int)player + 1;
 	printf("start : %d\n", proc->start);
 	ft_lstadd(&tmp.process, proc);
 	return (tmp);
