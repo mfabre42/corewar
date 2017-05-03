24e9f6fab9321dfc03066de10a62a353217e5787
Maxime FABRE <mafabre@e2r4p2.42.fr>
2017-05-03 19:37:50 +0200

    add function add, st, sub, zjmp

--- srcs/functions/func_st.c
+++ srcs/functions/func_st.c
@@ -6,7 +6,7 @@
 /*   By: mafabre <mafabre@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 15:34:09 by mafabre           #+#    #+#             */
-/*   Updated: 2017/05/03 16:56:16 by mafabre          ###   ########.fr       */
+/*   Updated: 2017/05/03 17:42:31 by mafabre          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,7 @@ void	direct_store(t_map *map, t_process *proc)
 	int		result;
 	t_ocp	ocp;
 
+
 	if (proc->do_funk == 1)
 	{
 		proc->cycle = 5;
@@ -30,6 +31,11 @@ void	direct_store(t_map *map, t_process *proc)
 		pos = proc->start + proc->pc;
 		ocp = ocp_master((int)map->arena[pos + 1]);
 		reg_nbr = (int)map->arena[pos + 2];
+		if (!is_register(reg_nbr))
+		{
+			printf("crash process a gerer st"); //LOL
+			exit(0);
+		}
 		if (ocp.param2 == 'R')
 		{
 			if (!is_register(hex_to_int(0x00, 0x00, 0x00, map->arena[pos + 3])))
@@ -39,5 +45,11 @@ void	direct_store(t_map *map, t_process *proc)
 			}
 			proc->reg[(int)map->arena[pos + 3]) - 1] =  proc->reg[reg_nbr - 1];
 			proc->pc = proc->pc + 4;
-	}
+		}
+		else
+		{
+			result = hex_to_int(0x00, 0x00, map->arena[pos + 3], map->arena[pos + 4]);
+			proc->pc = proc->pc + 5;
+		}
+		place_in_arena(map, pos + (result % IDX_MOD), proc->reg[reg_nbr - 1]);
 }
