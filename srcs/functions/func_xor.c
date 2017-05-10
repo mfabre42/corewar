19e65b2ddb1b5e563d4c8cc0e27d62eecba23a6a
Alexis LEVEQUE <aleveque@e2r6p7.42.fr>
2017-05-10 22:33:47 +0200

    modulo sur and or et xor

--- srcs/functions/func_xor.c
+++ srcs/functions/func_xor.c
@@ -6,7 +6,7 @@
 /*   By: aleveque <aleveque@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 19:57:24 by aleveque          #+#    #+#             */
-/*   Updated: 2017/05/10 15:39:55 by acoupleu         ###   ########.fr       */
+/*   Updated: 2017/05/10 22:31:51 by aleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,55 +28,55 @@ void	func_xor(t_map *map, t_process *proc)
 		do_funk(proc, 0, 0, 1);
 		pc = 2;
 		pos = proc->start + proc->pc;
-		ocp = ocp_master((int)map->arena[pos + 1]);
+		ocp = ocp_master((int)map->arena[(pos + 1) % MEM_SIZE]);
 		if (ocp.param1 == 'R')
 		{
-			if (!is_register(((int)map->arena[pos + pc])))
+			if (!is_register(((int)map->arena[(pos + pc) % MEM_SIZE])))
 			{
-				fail_func(proc, pc + 1, 1);
+				fail_func(proc, 3, 1);
 				return ;
 			}
-			param1 = proc->reg[(int)map->arena[pos + pc] - 1];
+			param1 = proc->reg[(int)map->arena[(pos + pc) % MEM_SIZE] - 1];
 			pc += 1;
 		}
 		else if (ocp.param1 == 'I')
 		{
-			param1 = hex_to_int(0x00, 0x00, map->arena[pos + 2], map->arena[pos + 3]);
+			param1 = hex_to_int(0x00, 0x00, map->arena[(pos + 2) % MEM_SIZE], map->arena[(pos + 3) % MEM_SIZE]);
 			pc += 2;
 		}
 		else if (ocp.param1 == 'D')
 		{
-			param1 = hex_to_int(map->arena[pos + 2], map->arena[pos + 3],
-				map->arena[pos + 4], map->arena[pos + 5]);
+			param1 = hex_to_int(map->arena[(pos + 2) % MEM_SIZE], map->arena[(pos + 3) % MEM_SIZE],
+				map->arena[(pos + 4) % MEM_SIZE], map->arena[(pos + 5) % MEM_SIZE]);
 			pc += 4;
 		}
 		if (ocp.param2 == 'R')
 		{
-			if (!is_register(((int)map->arena[pos + pc])))
+			if (!is_register(((int)map->arena[(pos + pc) % MEM_SIZE])))
 			{
 				fail_func(proc, pc + 1, 1);
 				return ;
 			}
-			param1 = param1 ^ proc->reg[(int)map->arena[pos + pc] - 1];
+			param1 = param1 ^ proc->reg[(int)map->arena[(pos + pc) % MEM_SIZE] - 1];
 			pc += 1;
 		}
 		else if (ocp.param2 == 'I')
 		{
-			param1 = param1 ^ hex_to_int(0x00, 0x00, map->arena[pos + pc], map->arena[pos + pc + 1]);
+			param1 = param1 ^ hex_to_int(0x00, 0x00, map->arena[(pos + pc) % MEM_SIZE], map->arena[(pos + pc + 1) % MEM_SIZE]);
 			pc += 2;
 		}
 		else if (ocp.param2 == 'D')
 		{
-			param1 = param1 ^ hex_to_int(map->arena[pos + pc], map->arena[pos + pc + 1],
-				map->arena[pos + pc + 2], map->arena[pos + pc + 3]);
+			param1 = param1 ^ hex_to_int(map->arena[(pos + pc) % MEM_SIZE], map->arena[(pos + pc + 1) % MEM_SIZE],
+				map->arena[(pos + pc + 2) % MEM_SIZE], map->arena[(pos + pc + 3) % MEM_SIZE]);
 			pc += 4;
 		}
-		if (!is_register(((int)map->arena[pos + pc])))
+		if (!is_register(((int)map->arena[(pos + pc) % MEM_SIZE])))
 		{
 			fail_func(proc, pc + 1, 1);
 			return ;
 		}
-		proc->reg[(int)map->arena[pos + pc] - 1] = param1;
+		proc->reg[(int)map->arena[(pos + pc) % MEM_SIZE] - 1] = param1;
 		proc->pc += pc + 1;
 		proc->carry = 1;
 	}
