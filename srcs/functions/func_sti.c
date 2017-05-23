a267b5e43c136c60732366f67e1890f78dfe9aac
Alexis LEVEQUE <aleveque@e2r10p7.42.fr>
2017-05-24 01:25:03 +0200

    norme

--- srcs/functions/func_sti.c
+++ srcs/functions/func_sti.c
@@ -6,11 +6,29 @@
 /*   By: acoupleu <acoupleu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/27 18:15:10 by acoupleu          #+#    #+#             */
-/*   Updated: 2017/05/24 01:15:26 by acoupleu         ###   ########.fr       */
+/*   Updated: 2017/05/24 01:24:34 by aleveque         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
+
+int		indirect_store3(t_map *map, t_process *proc, t_bin *bin, int *result)
+{
+	else if ((POCP.param2 == 'D' || POCP.param2 == 'I') && POCP.param3 == 'D')
+	{
+		result = (short)hex_to_int(0x00, 0x00, map->arena[(PPOS + 3) % MEM_SIZE], map->arena[(PPOS + 4) % MEM_SIZE]);
+		result += (short)hex_to_int(0x00, 0x00, map->arena[(PPOS + 5) % MEM_SIZE], map->arena[(PPOS + 6) % MEM_SIZE]);
+		proc->pc = proc->pc + 7;
+	}
+	else if ((POCP.param2 == 'D' || POCP.param2 == 'I') && POCP.param3 == 'R')
+	{
+		if (!is_reg((int)map->arena[(PPOS + 5) % MEM_SIZE], proc, 6))
+			return ;
+		result = (short)hex_to_int(0x00, 0x00, map->arena[(PPOS + 3) % MEM_SIZE], map->arena[(PPOS + 4) % MEM_SIZE]);
+		result += proc->reg[(int)map->arena[(PPOS + 5) % MEM_SIZE] - 1];
+		proc->pc = proc->pc + 6;
+	}
+}
 
 int		indirect_store2(t_map *map, t_process *proc, t_bin *bin, int *result)
 {
@@ -46,8 +64,6 @@ void	indirect_store(t_map *map, t_process *proc)
 	{
 		do_funk(proc, 0, 0, 1);
 		bin = init_bin(map, proc);
-		PPOS = proc->start + proc->pc;
-		POCP = ocp_master((int)map->arena[(PPOS + 1) % MEM_SIZE]);
 		if (!is_reg((int)map->arena[(PPOS + 2) % MEM_SIZE], proc, 5))
 			return ;
 		PREG_NBR = (int)map->arena[(PPOS + 2) % MEM_SIZE];
@@ -56,8 +72,10 @@ void	indirect_store(t_map *map, t_process *proc)
 			if (indirect_store2(map, proc, &bin, &result) == 0)
 				return ;
 		}
-		else if ((POCP.param2 == 'D' || POCP.param2 == 'I') && POCP.param3 == 'D')
+		else if ((POCP.param2 == 'D' || POCP.param2 == 'I') && (POCP.param3 == 'D' || POCP.param3 == 'R'))
 		{
+			if (indirect_store2(map, proc, &bin, &result) == 0)
+				return ;
 			result = (short)hex_to_int(0x00, 0x00, map->arena[(PPOS + 3) % MEM_SIZE], map->arena[(PPOS + 4) % MEM_SIZE]);
 			result += (short)hex_to_int(0x00, 0x00, map->arena[(PPOS + 5) % MEM_SIZE], map->arena[(PPOS + 6) % MEM_SIZE]);
 			proc->pc = proc->pc + 7;
