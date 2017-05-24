197f309c79071e6cabf7e1f9cd28eca57e303572
acoupleu <acoupleu@e2r9p6.42.fr>
2017-05-24 02:07:58 +0200

    norme

--- srcs/functions/func_ldi.c
+++ srcs/functions/func_ldi.c
@@ -6,17 +6,74 @@
 /*   By: acoupleu <acoupleu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/03 15:34:48 by acoupleu          #+#    #+#             */
-/*   Updated: 2017/05/24 01:26:43 by acoupleu         ###   ########.fr       */
+/*   Updated: 2017/05/24 02:02:00 by acoupleu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	indirect_load(t_map *map, t_process *proc)
+static int	indirect_load3(t_map *map, t_process *proc, t_bin *bin, int *result)
 {
-	int		pos;
+	if ((OCP.param1 == 'D' || OCP.param1 == 'I') && OCP.param2 == 'R')
+	{
+		if (!is_reg((int)ARENA[(POS + 4) % MEM_SIZE], proc, 6)
+		|| !is_reg((int)ARENA[(POS + 5) % MEM_SIZE], proc, 6))
+			return (0);
+		*result = (short)hex_to_int(0x00, 0x00, ARENA[(POS + 2) % MEM_SIZE],
+		ARENA[(POS + 3) % MEM_SIZE]);
+		*result += proc->reg[(int)ARENA[(POS + 4) % MEM_SIZE] - 1];
+		proc->reg[(int)ARENA[(POS + 5) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + (*result % IDX_MOD)) % MEM_SIZE);
+		proc->pc = proc->pc + 6;
+	}
+	else if ((OCP.param1 == 'D' || OCP.param1 == 'I') && OCP.param2 == 'D')
+	{
+		if (!is_reg((int)ARENA[(POS + 6) % MEM_SIZE], proc, 7))
+			return (0);
+		*result = (short)hex_to_int(0x00, 0x00, ARENA[(POS + 2) % MEM_SIZE],
+		ARENA[(POS + 3) % MEM_SIZE]);
+		*result += (short)hex_to_int(0x00, 0x00, ARENA[(POS + 4) % MEM_SIZE],
+		ARENA[(POS + 5) % MEM_SIZE]);
+		proc->reg[(int)ARENA[(POS + 6) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + (*result % IDX_MOD)) % MEM_SIZE);
+		proc->pc = proc->pc + 7;
+	}
+	return (1);
+}
+
+static int	indirect_load2(t_map *map, t_process *proc, t_bin *bin, int *result)
+{
+	if (OCP.param1 == 'R' && OCP.param2 == 'R')
+	{
+		if (!is_reg((int)ARENA[(POS + 2) % MEM_SIZE], proc, 5)
+		|| !is_reg((int)ARENA[(POS + 3) % MEM_SIZE], proc, 5)
+		|| !is_reg((int)ARENA[(POS + 4) % MEM_SIZE], proc, 5))
+			return (0);
+		*result = proc->reg[(int)ARENA[(POS + 2) % MEM_SIZE] - 1];
+		*result += proc->reg[(int)ARENA[(POS + 3) % MEM_SIZE] - 1];
+		proc->reg[(int)ARENA[(POS + 4) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + (*result % IDX_MOD)) % MEM_SIZE);
+		proc->pc = proc->pc + 5;
+	}
+	else if (OCP.param1 == 'R' && OCP.param2 == 'D')
+	{
+		if (!is_reg((int)ARENA[(POS + 2) % MEM_SIZE], proc, 6)
+		|| !is_reg((int)ARENA[(POS + 5) % MEM_SIZE], proc, 6))
+			return (0);
+		*result = proc->reg[(int)ARENA[(POS + 2) % MEM_SIZE] - 1];
+		*result += (short)hex_to_int(0x00, 0x00, ARENA[(POS + 3) % MEM_SIZE],
+		ARENA[(POS + 4) % MEM_SIZE]);
+		proc->reg[(int)ARENA[(POS + 5) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + (*result % IDX_MOD)) % MEM_SIZE);
+		proc->pc = proc->pc + 6;
+	}
+	return (1);
+}
+
+void		indirect_load(t_map *map, t_process *proc)
+{
+	t_bin	bin;
 	int		result;
-	t_ocp	ocp;
 
 	if (proc->do_funk == 1)
 	{
@@ -25,47 +82,17 @@ void	indirect_load(t_map *map, t_process *proc)
 	else
 	{
 		do_funk(proc, 0, 0, 1);
-		pos = proc->start + proc->pc;
-		ocp = ocp_master((int)ARENA[(pos + 1) % MEM_SIZE]);
-		if (ocp.param1 == 'R' && ocp.param2 == 'R')
+		bin = init_bin(map, proc);
+		if (POCP.param1 == 'R' && (POCP.param2 == 'R' || POCP.param2 == 'D'))
 		{
-			if (!is_reg((int)ARENA[(pos + 2) % MEM_SIZE], proc, 5)
-			|| !is_reg((int)ARENA[(pos + 3) % MEM_SIZE], proc, 5)
-			|| !is_reg((int)ARENA[(pos + 4) % MEM_SIZE], proc, 5))
+			if (indirect_load2(map, proc, &bin, &result) == 0)
 				return ;
-			result = proc->reg[(int)ARENA[(pos + 2) % MEM_SIZE] - 1];
-			result += proc->reg[(int)ARENA[(pos + 3) % MEM_SIZE] - 1];
-			proc->reg[(int)ARENA[(pos + 4) % MEM_SIZE] - 1] = read_in_arena(map, (pos + (result % IDX_MOD)) % MEM_SIZE);
-			proc->pc = proc->pc + 5;
 		}
-		else if (ocp.param1 == 'R' && ocp.param2 == 'D')
+		else if ((POCP.param1 == 'D' || POCP.param1 == 'I')
+				&& (POCP.param2 == 'R' || POCP.param2 == 'D'))
 		{
-			if (!is_reg((int)ARENA[(pos + 2) % MEM_SIZE], proc, 6)
-			|| !is_reg((int)ARENA[(pos + 5) % MEM_SIZE], proc, 6))
+			if (indirect_load3(map, proc, &bin, &result) == 0)
 				return ;
-			result = proc->reg[(int)ARENA[(pos + 2) % MEM_SIZE] - 1];
-			result += (short)hex_to_int(0x00, 0x00, ARENA[(pos + 3) % MEM_SIZE], ARENA[(pos + 4) % MEM_SIZE]);
-			proc->reg[(int)ARENA[(pos + 5) % MEM_SIZE] - 1] = read_in_arena(map, (pos + (result % IDX_MOD)) % MEM_SIZE);
-			proc->pc = proc->pc + 6;
-		}
-		else if ((ocp.param1 == 'D' || ocp.param1 == 'I') && ocp.param2 == 'R')
-		{
-			if (!is_reg((int)ARENA[(pos + 4) % MEM_SIZE], proc, 6)
-			|| !is_reg((int)ARENA[(pos + 5) % MEM_SIZE], proc, 6))
-				return ;
-			result = (short)hex_to_int(0x00, 0x00, ARENA[(pos + 2) % MEM_SIZE], ARENA[(pos + 3) % MEM_SIZE]);
-			result += proc->reg[(int)ARENA[(pos + 4) % MEM_SIZE] - 1];
-			proc->reg[(int)ARENA[(pos + 5) % MEM_SIZE] - 1] = read_in_arena(map, (pos + (result % IDX_MOD)) % MEM_SIZE);
-			proc->pc = proc->pc + 6;
-		}
-		else if ((ocp.param1 == 'D' || ocp.param1 == 'I') && ocp.param2 == 'D')
-		{
-			if (!is_reg((int)ARENA[(pos + 6) % MEM_SIZE], proc, 7))
-				return ;
-			result = (short)hex_to_int(0x00, 0x00, ARENA[(pos + 2) % MEM_SIZE], ARENA[(pos + 3) % MEM_SIZE]);
-			result += (short)hex_to_int(0x00, 0x00, ARENA[(pos + 4) % MEM_SIZE], ARENA[(pos + 5) % MEM_SIZE]);
-			proc->reg[(int)ARENA[(pos + 6) % MEM_SIZE] - 1] = read_in_arena(map, (pos + (result % IDX_MOD)) % MEM_SIZE);
-			proc->pc = proc->pc + 7;
 		}
 	}
 }
--- srcs/functions/func_lldi.c
+++ srcs/functions/func_lldi.c
@@ -6,72 +6,93 @@
 /*   By: acoupleu <acoupleu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/05/04 15:42:36 by acoupleu          #+#    #+#             */
-/*   Updated: 2017/05/24 01:26:43 by acoupleu         ###   ########.fr       */
+/*   Updated: 2017/05/24 02:06:05 by acoupleu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "corewar.h"
 
-void	lindirect_load(t_map *map, t_process *proc)
+static int	lindirect_load3(t_map *map, t_process *proc, t_bin *bin, int *result)
 {
-	int		pos;
+	if ((OCP.param1 == 'D' || OCP.param1 == 'I') && OCP.param2 == 'R')
+	{
+		if (!is_reg((int)ARENA[(POS + 4) % MEM_SIZE], proc, 6)
+		|| !is_reg((int)ARENA[(POS + 5) % MEM_SIZE], proc, 6))
+			return (0);
+		*result = (short)hex_to_int(0x00, 0x00, ARENA[(POS + 2) % MEM_SIZE],
+		ARENA[(POS + 3) % MEM_SIZE]);
+		*result += proc->reg[(int)ARENA[(POS + 4) % MEM_SIZE] - 1];
+		proc->reg[(int)ARENA[(POS + 5) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + *result) % MEM_SIZE);
+		proc->pc = proc->pc + 6;
+	}
+	else if ((OCP.param1 == 'D' || OCP.param1 == 'I') && OCP.param2 == 'D')
+	{
+		if (!is_reg((int)ARENA[(POS + 6) % MEM_SIZE], proc, 7))
+			return (0);
+		*result = (short)hex_to_int(0x00, 0x00, ARENA[(POS + 2) % MEM_SIZE],
+		ARENA[(POS + 3) % MEM_SIZE]);
+		*result += (short)hex_to_int(0x00, 0x00, ARENA[(POS + 4) % MEM_SIZE],
+		ARENA[(POS + 5) % MEM_SIZE]);
+		proc->reg[(int)ARENA[(POS + 6) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + *result) % MEM_SIZE);
+		proc->pc = proc->pc + 7;
+	}
+	return (1);
+}
+
+static int	lindirect_load2(t_map *map, t_process *proc, t_bin *bin, int *result)
+{
+	if (OCP.param1 == 'R' && OCP.param2 == 'R')
+	{
+		if (!is_reg((int)ARENA[(POS + 2) % MEM_SIZE], proc, 5)
+		|| !is_reg((int)ARENA[(POS + 3) % MEM_SIZE], proc, 5)
+		|| !is_reg((int)ARENA[(POS + 4) % MEM_SIZE], proc, 5))
+			return (0);
+		*result = proc->reg[(int)ARENA[(POS + 2) % MEM_SIZE] - 1];
+		*result += proc->reg[(int)ARENA[(POS + 3) % MEM_SIZE] - 1];
+		proc->reg[(int)ARENA[(POS + 4) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + *result) % MEM_SIZE);
+		proc->pc = proc->pc + 5;
+	}
+	else if (OCP.param1 == 'R' && OCP.param2 == 'D')
+	{
+		if (!is_reg((int)ARENA[(POS + 2) % MEM_SIZE], proc, 6)
+		|| !is_reg((int)ARENA[(POS + 5) % MEM_SIZE], proc, 6))
+			return (0);
+		*result = proc->reg[(int)ARENA[(POS + 2) % MEM_SIZE] - 1];
+		*result += (short)hex_to_int(0x00, 0x00, ARENA[(POS + 3) % MEM_SIZE],
+		ARENA[(POS + 4) % MEM_SIZE]);
+		proc->reg[(int)ARENA[(POS + 5) % MEM_SIZE] - 1] =
+		read_in_arena(map, (POS + *result) % MEM_SIZE);
+		proc->pc = proc->pc + 6;
+	}
+	return (1);
+}
+
+void		lindirect_load(t_map *map, t_process *proc)
+{
+	t_bin	bin;
 	int		result;
-	t_ocp	ocp;
 
 	if (proc->do_funk == 1)
 	{
-		do_funk(proc, 48, 14, 0);
+		do_funk(proc, 23, 14, 0);
 	}
 	else
 	{
 		do_funk(proc, 0, 0, 1);
-		pos = proc->start + proc->pc;
-		ocp = ocp_master((int)ARENA[(pos + 1) % MEM_SIZE]);
-		if (ocp.param1 == 'R' && ocp.param2 == 'R')
+		bin = init_bin(map, proc);
+		if (POCP.param1 == 'R' && (POCP.param2 == 'R' || POCP.param2 == 'D'))
 		{
-			if (!is_reg((int)ARENA[(pos + 2) % MEM_SIZE], proc, 5)
-			|| !is_reg((int)ARENA[(pos + 3) % MEM_SIZE], proc, 5)
-			|| !is_reg((int)ARENA[(pos + 4) % MEM_SIZE], proc, 5))
+			if (lindirect_load2(map, proc, &bin, &result) == 0)
 				return ;
-			result = (pos + ((proc->reg[(int)ARENA[(pos + 2) % MEM_SIZE] - 1]) % 3)) % MEM_SIZE;
-			result += proc->reg[(int)ARENA[(pos + 3) % MEM_SIZE] - 1];
-			proc->reg[(int)ARENA[(pos + 4) % MEM_SIZE] - 1] = read_in_arena(map, (pos + result) % MEM_SIZE);
-			proc->pc = proc->pc + 5;
 		}
-		else if (ocp.param1 == 'R' && ocp.param2 == 'D')
+		else if ((POCP.param1 == 'D' || POCP.param1 == 'I')
+				&& (POCP.param2 == 'R' || POCP.param2 == 'D'))
 		{
-			if (!is_reg((int)ARENA[(pos + 2) % MEM_SIZE], proc, 6)
-			|| !is_reg((int)ARENA[(pos + 5) % MEM_SIZE], proc, 6))
+			if (lindirect_load3(map, proc, &bin, &result) == 0)
 				return ;
-			result = (pos + ((proc->reg[(int)ARENA[(pos + 2) % MEM_SIZE] - 1]) % 3)) % MEM_SIZE;
-			result += (short)hex_to_int(0x00, 0x00, ARENA[(pos + 3) % MEM_SIZE], ARENA[(pos + 4) % MEM_SIZE]);
-			proc->reg[(int)ARENA[(pos + 5) % MEM_SIZE] - 1] = read_in_arena(map, (pos + result) % MEM_SIZE);
-			proc->pc = proc->pc + 6;
 		}
-		else if ((ocp.param1 == 'D' || ocp.param1 == 'I') && ocp.param2 == 'R')
-		{
-			if (!is_reg((int)ARENA[(pos + 4) % MEM_SIZE], proc, 6)
-			|| !is_reg((int)ARENA[(pos + 5) % MEM_SIZE], proc, 6))
-				return ;
-			result = (pos + ((short)hex_to_int(0x00, 0x00, ARENA[(pos + 2) % MEM_SIZE], ARENA[(pos + 3) % MEM_SIZE]) % 3)) % MEM_SIZE;
-			result += proc->reg[(int)ARENA[(pos + 4) % MEM_SIZE] - 1];
-			proc->reg[(int)ARENA[(pos + 5) % MEM_SIZE] - 1] = read_in_arena(map, (pos + result) % MEM_SIZE);
-			proc->pc = proc->pc + 6;
-		}
-		else if ((ocp.param1 == 'D' || ocp.param1 == 'I') && ocp.param2 == 'D')
-		{
-			if (!is_reg((int)ARENA[(pos + 6) % MEM_SIZE], proc, 7))
-				return ;
-			result = (pos + ((short)hex_to_int(0x00, 0x00, ARENA[(pos + 2) % MEM_SIZE], ARENA[(pos + 3) % MEM_SIZE]) % 3 )) % MEM_SIZE;
-			result += (short)hex_to_int(0x00, 0x00, ARENA[(pos + 4) % MEM_SIZE], ARENA[(pos + 5) % MEM_SIZE]);
-			proc->reg[(int)ARENA[(pos + 6) % MEM_SIZE] - 1] = read_in_arena(map, (pos + result) % MEM_SIZE);
-			proc->pc = proc->pc + 7;
-		}
-		else
-		{
-			proc->pc = proc->pc + 5;
-			return ;
-		}
-		proc->carry = !proc->reg[(int)ARENA[(pos + 6) % MEM_SIZE] - 1];
 	}
 }
