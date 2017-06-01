1739ab4b3b1d6923119a9362986cf218ed1e033e
Alexis LEVEQUE <aleveque@e2r5p3.42.fr>
2017-06-01 19:16:38 +0200

    champ de ouf

--- lbich.s
+++ lbich.s
@@ -1,9 +1,26 @@
-.name "lbich"
+.name "lbich le conquerant"
 .comment "j'ai eu un degat des eaux"
 
-sti:	sti r1, %:livi, %1
-		and r4, %0, r3
+aff: ld %82,r2
+	aff r2
+	ld %69,r2
+	aff r2
+	ld %75,r2
+	aff r2
+	ld %84,r2
+	aff r2
+	ld %32,r2
+	aff r2
+	ld %90,r2
+	aff r2
+	ld %79,r2
+	aff r2
+	ld %82,r2
+	aff r2
+	ld %75,r2
+	aff r2
+	ld %32,r2
+	aff r2
+	and r3, r4, r5
 
-livi:		live %1
-
-live:	zjmp %:livi
+live:	zjmp %480
